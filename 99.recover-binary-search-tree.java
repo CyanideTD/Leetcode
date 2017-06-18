2b8f516e394b9cfe5d37df254dbab483303ce11c
CyanideTD <qchen10@stevens.edu>
2017-06-18 15:23:13 +0100

    06/18

--- 99.recover-binary-search-tree.java
+++ 99.recover-binary-search-tree.java
@@ -8,7 +8,35 @@
  * }
  */
 public class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
     public void recoverTree(TreeNode root) {
-        
+    	if (root == null) {
+		return;
+	}
+	traverse(root);
+	int temp = first.val;
+	first.val = second.val;
+	second.val = temp;
     }
+    public void traverse(TreeNode root) {
+    	if (root == null) {
+		return;
+	}
+	traverse(root.left);
+	if (prev != null) {
+		if (root.val < prev.val) {
+			if (first == null) {
+				first = prev;
+				second = root;
+			} else {
+				second = root;
+			}
+		}
+	}
+	prev = root;
+	traverse(root.right);
+    }
+
 }
